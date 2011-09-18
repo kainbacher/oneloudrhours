f2442145c0fce230ba3b4bc0826a2a3f59f7097f
Roland Kainbacher <rka@beautyparlour.at>
2011-09-18 21:27:27 +0200

    dj 13

--- urls.py
+++ urls.py
@@ -15,12 +15,12 @@ urlpatterns = patterns('',
     # (r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
     # Uncomment the next line to enable the admin:
-    (r'^admin/(.*)', admin.site.root),
-    #(r'^admin/', include(admin.site.urls)),
+    #(r'^admin/(.*)', admin.site.root),
+    (r'^admin/', include(admin.site.urls)),
     
 #    (r'^', include('oneloudrhours.main.urls')),
 
-    (r'^$', admin.site.root),
+    #(r'^$', admin.site.root),
 )
 
 
