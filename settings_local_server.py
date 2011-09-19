ef61115da5c7219bbe730f398f4c88951cdc2836
Roland Kainbacher <rka@beautyparlour.at>
2011-09-19 09:57:40 +0200

    deleted:    settings_local_bak.py 	modified:   settings_local_server.py 	modified:   settings_website.py

--- settings_local_server.py
+++ settings_local_server.py
@@ -13,13 +13,13 @@ DEBUG = True
 TEMPLATE_DEBUG = DEBUG
 
 DATABASE_ENGINE = 'postgresql_psycopg2'           # 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 
-DATABASE_NAME = 'db1797_captain'           # Or path to database file if using sqlite3.
+DATABASE_NAME = 'db1797_oneloudrhours'           # Or path to database file if using sqlite3.
 DATABASE_USER = 'beautyparlour'             # Not used with sqlite3.
 DATABASE_PASSWORD = 'ZfSpHvYz'         # Not used with sqlite3.
 DATABASE_HOST = 'external-db.s1797.gridserver.com'             # Set to empty string for localhost. Not used with 
 DATABASE_PORT = ''             # Set to empty string for default. Not used with sqlite3.
 
-URL = 'http://captain.beautyparlour.at/'
+URL = 'http://oneloudrhours.beautyparlour.at/'
 
 #google maps key
 GOOGLE_KEY = 'ABQIAAAAwW8e4vOGD-0EcKHxxa0_QhQCULP4XOMyhPd8d_NrQQEO8sT8XBRwAjay1jJRh1Zq73pRghRFR_6lAg'
